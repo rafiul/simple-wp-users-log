f93d631795a15049990f5e2ddbb20c750707b939
B.M. Rafiul Alam <bmrafiul.alam@gmail.com>
2019-11-11 16:47:56 +0600

    Update init.php

--- test-user-logs/init.php
+++ test-user-logs/init.php
@@ -4,12 +4,12 @@ Plugin Name: Test User Logs
 Plugin URI:https://wordpress.org/plugins/
 Description: A simple user log plugins.
 Author: B.M. Rafiul Alam
-Author URI: https://themeforest.net/user/3rtheme/portfolio
+Author URI: https://themeforest.net/user/themezbyte/portfolio
 Text Domain: ra-wp
 Version: 1.0
 */
 
-//======== Enque Library ==========
+//======== Admin Menu ==========
 add_action('admin_menu', 'user_log_menu');
 	function user_log_menu() {
 		add_menu_page( 'User Logs', 'User Logs', 'manage_options', 'user-list.php', 'user_logs', 'dashicons-admin-users', 50  );
